7b65b872be95f20f6d47c4ee821d90be4f68daea
idowu007 <adeyinkaidowu007@gmail.com.com>
2023-06-21 17:18:20 -0700

    0x02 func. task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * print_alphabet - print lowercase alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
