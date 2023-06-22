1f28185b87f647165346275b8e7a1406f27b0fe0
idowu007 <adeyinkaidowu007@gmail.com.com>
2023-06-22 03:51:05 -0700

    0x02 function task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - print 9 times table
+ */
+
+void times_table(void)
+{
+	int row, col, result;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			result = (row * col);
+
+			if (col == 0)
+			{
+				_putchar('0' + result);
+			}
+			else if (result <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + result);
+			}
+			else if (result > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (result / 10));
+				_putchar('0' + (result % 10));
+			}
+		}
+		_putchar('\n');
+	}
+}
