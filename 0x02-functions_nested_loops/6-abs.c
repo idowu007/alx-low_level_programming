349207b6e144ddb5ad9f97a5130a90bac28251f1
idowu007 <adeyinkaidowu007@gmail.com.com>
2023-06-21 19:22:01 -0700

    0x02 function task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - print the absolute valur of the integer
+ * @n: integer to check
+ * Return: value
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	} else if (n < 0)
+	{
+		return (-n);
+	}
+
+		return (0);
+}
