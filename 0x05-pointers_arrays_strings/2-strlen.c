e00f5d9293a3f71dfadbf8cacc848a5127fb246a
idowu007 <adeyinkaidowu007@gmail.com.com>
2023-06-28 08:17:38 -0700

    0x05 task 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of a string
+ * @s: string involved
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
